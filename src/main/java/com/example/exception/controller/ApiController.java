1274df737b1cd4d838db2dacacb4e4db82c2069b
kimjaeseop <dkfzm3221@naver.com>
2021-12-29 16:27:37 +0900

    2021-12-29 commit

--- src/main/java/com/example/exception/controller/ApiController.java
+++ src/main/java/com/example/exception/controller/ApiController.java
@@ -3,18 +3,29 @@ package com.example.exception.controller;
 import com.example.exception.dto.User;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 
 @RestController
 @RequestMapping("/api/user")
+@Validated
 public class ApiController {
 
     @GetMapping("")
-    public User get(@RequestParam(required = false) String name, @RequestParam(required = false) Integer age){
+    public User get(
+            @Size(min = 2)
+            @RequestParam(required = false) String name,
+
+            @NotNull
+            @Min(1)
+            @RequestParam(required = false) Integer age){
 
         User user = new User();
         user.setName(name);
@@ -41,4 +52,5 @@ public class ApiController {
         System.out.println(("api controller"));
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
     }
+
 }
